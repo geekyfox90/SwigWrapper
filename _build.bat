96cb92b094b6237a48ca9baa314149cecdf7a8d0
BCOM\tlesaunier <thibault.lesaunier@b-com.com>
2019-05-14 14:32:43 +0200

    better bat files - call PipelineManager bat

--- _build.bat
+++ _build.bat
@@ -1,6 +1,7 @@
-mkdir "%BCOMDEVROOT%/bcomBuild/SolARPipelineManager/0.5.2/CSharp"
-del /s "%BCOMDEVROOT%\bcomBuild\SolARPipelineManager\0.5.2\CSharp\*.*"
-swig -csharp -namespace SolAR -c++ -fcompact -small -O -Iswig -I%BCOMDEVROOT%/thirdParties/xpcf/2.1.0/interfaces -I%BCOMDEVROOT%/bcomBuild/SolARFramework/0.5.2/interfaces -DSWIG_CSHARP_NO_WSTRING_HELPER -outdir "%BCOMDEVROOT%/bcomBuild/SolARPipelineManager/0.5.2/CSharp" -o "../../SolARPipelineManager/src/PipelineManager_wrap.cpp" ../../SolARPipelineManager/interfaces/SolARPipelineManager.i
+SET CURRENTDIR="%cd%"
+cd ..\..\SolARPipelineManager\
+call BuildCSharp.bat
+cd %CURRENTDIR%
 
 ::CLS
 @ECHO OFF
